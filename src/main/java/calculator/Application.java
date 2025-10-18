8f2978b9eeb170e27e482eb4dac2c08c7284c713
eclipse021 <yahang2008@naver.com>
2025-10-19 03:15:43 +0900

    feat: 테스트 포맷에 맞게 main 함수 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,8 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         System.out.println("결과 : " + new Calculator().calculator());
 
     }
