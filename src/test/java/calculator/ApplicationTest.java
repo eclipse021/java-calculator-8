8cde009ecfe2ed9abceca7fb8fa5bc09deec6a0f
eclipse021 <yahang2008@naver.com>
2025-10-19 02:28:04 +0900

    test: 숫자합이 제대로 계산되는지 테스트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,6 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 숫자합_테스트(){
+        assertSimpleTest(() -> {
+            run("12:13:14");
+            assertThat(output().contains("결과 : 39"));
+        });
+    }
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
