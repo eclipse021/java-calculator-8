32b297d19def6e3e8474d2244dd538aae0530eda
eclipse021 <yahang2008@naver.com>
2025-10-20 01:16:48 +0900

    refactor: SRP 원칙에 의거해 입력과 출력을 담당할 수 있는 클래스 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,13 @@
 package calculator.view;
 
+import calculator.domain.calculator.model.dto.Input;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static Input inputView() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String raw = Console.readLine();
+        return new Input(raw);
+    }
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,9 @@
 package calculator.view;
 
 public class OutputView {
+
+    public static void outputView(int result) {
+        System.out.println("결과 : " + result);
+    }
+
 }
